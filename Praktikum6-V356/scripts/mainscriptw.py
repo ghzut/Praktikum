0caccd7cd0727170281ddcd0ff797245291fbb90
Lukas Rolf <lukas.rolf@tu-dortmund.de>
2016-12-02 17:05:48 +0100

    Keine Ahnung wie man theta bei b berechnen soll da nicht bekannt ist das wievielfache von pi man bei den frequenzen jeweils gemessen hat.

--- Praktikum6-V356/scripts/mainscriptw.py
+++ Praktikum6-V356/scripts/mainscriptw.py
@@ -1,4 +1,5 @@
-
+from table import makeTable
+from bereich import bereich
 import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
@@ -24,25 +25,25 @@ import uncertainties.unumpy as unp
 #nichts
 
 
-def f(t, w, U):
-	return U*np.exp(-w*t)
+#def f(t, w, U):
+#	return U*np.exp(-w*t)
 
 #x, y = np.genfromtxt('content/aufgabendatena', unpack=True)
 #makeTable([x, y], [r'$\Delta t/\si{\micro\second}$', r'$A_C/\si{\milli\volt}$'], 'Messwerte zu Versuchsteil a).', 'taba', ['3.1', '3.1'])
-namex, namey = [r'$\Delta t/\si{\micro\second}$', r'$A_C/\si{\milli\volt}$']
+#namex, namey = [r'$\Delta t/\si{\micro\second}$', r'$A_C/\si{\milli\volt}$']
 #params, covar = curve_fit(f, x, y)
-plt.cla()
-plt.clf()
-t = np.linspace(0, 10000, 100000)
+#plt.cla()
+#plt.clf()
+#t = np.linspace(0, 10000, 100000)
 #print(params, covar, sep='\n')
 #plt.plot(x, y, 'rx', label='Daten')
-plt.plot(t, f(t, 1, 1), 'b-', label='Fit')
+#plt.plot(t, f(t, 1, 1), 'b-', label='Fit')
 #plt.xlim(x[0], x[-1])
-plt.xlabel(namex)
-plt.ylabel(namey)
-plt.legend(loc='best')
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/'+'graa')
+#plt.xlabel(namex)
+#plt.ylabel(namey)
+#plt.legend(loc='best')
+#plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+#plt.savefig('build/'+'graa')
 #gamma = unp.uarray(params[0], np.sqrt(covar[0][0]))
 #U = unp.uarray(params[1], np.sqrt(covar[1][1]))
 
@@ -71,13 +72,30 @@ def w(theta, L, C):
 	return np.sqrt(2/(L*C) * (1-np.cos(theta)))
 
 
-f = genfromtxt('datenb', unpack = True)
+f1, f2 = np.genfromtxt('scripts/datenb', unpack = True)
+n = 0;
+N = 16;
+omega1 = []
+for freq1 in f1:
+	omega1.append(np.pi*n/N)
+	n = n + 1
+omega1 = np.array(omega1)
+print(omega1)
+n = 0;
+N = 16;
+omega2 = []
+for freq2 in f2:
+	omega2.append(np.pi*n/(N))
+	n = n + 1
+omega2 = np.array(omega2)
+print(omega2)
 
 namex, namey = [r'$\theta$', r'$\omega/\si{\per\second}$']
 t = np.linspace(0, np.pi/2, 100000)
 plt.cla()
 plt.clf()
-plt.plot(t, w(t, L, 2*C1), 'y-', label='Theorie w')
+plt.plot(omega1, f1/(2*np.pi), 'rx', label='Daten')
+plt.plot(t, w(t, L, 2*C1), 'b-', label='Theorie')
 plt.xlabel(namex)
 plt.ylabel(namey)
 plt.legend(loc='best')
@@ -85,8 +103,9 @@ plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.savefig('build/'+'grab1')
 plt.cla()
 plt.clf()
-plt.plot(t, w2(t, L, C1, C2), 'b-', label='Theorie w2')
-plt.plot(t, w1(t, L, C1, C2), 'r-', label='Theorie w1')
+plt.plot(omega2, f2/(2*np.pi), 'rx', label='Daten')
+plt.plot(t, w2(t, L, C1, C2), 'b-', label='Theorie')
+plt.plot(t, w1(t, L, C1, C2), 'b-', label='Theorie')
 plt.xlabel(namex)
 plt.ylabel(namey)
 plt.legend(loc='best')
