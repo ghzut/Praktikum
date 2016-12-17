e3e08677577f3dcf0d0dc518e1299de2e6adfb1d
Lukas Rolf <lukas.rolf@tu-dortmund.de>
2016-12-17 15:06:29 +0100

    Bessere Tabellem mit Überschrieften.

--- Praktikum8-V103/scripts/table.py
+++ Praktikum8-V103/scripts/table.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def makeTable(data, names, name, filename, formats):
+def makeTable(data, names, name, filename, formats=[], formats2=[]):
     TableFile = open('build/'+filename+'.tex', 'w+')
     TableFile.write(r'\begin{table}'+'\n\t'+r'\centering'+'\n\t'+r'\caption{'+name+r'}'+'\n\t'+r'\label{tab:'+filename+'}\n\t'+r'\sisetup{table-format=1.2}'+'\n\t'+r'\begin{tabular}{')
     for i in range(len(data)):
@@ -10,16 +10,22 @@ def makeTable(data, names, name, filename, formats):
             TableFile.write('S ')
     TableFile.write('}\n\t\t')
     TableFile.write(r'\toprule'+'\n\t\t')
-    
+
     TableFile.write(names)
 
     TableFile.write(r' \\'+'\n\t\t')
     TableFile.write(r'\midrule'+'\n\t\t')
     for i in range(len(data[0])):
-        for value in data[0:-1]:
-            TableFile.write(str(value[i]))
+        for b in range(len(data[0:-1])):
+            if formats2:
+                TableFile.write(formats2[b] % (data[b][i]))
+            else:
+                TableFile.write(str(data[b][i]))
             TableFile.write(r' & ')
-        TableFile.write(str(data[-1][i]))
+        if formats2:
+            TableFile.write(formats2[-1] % (data[-1][i]))
+        else:
+            TableFile.write(str(data[-1][i]))
         TableFile.write(r' \\')
         TableFile.write('\n\t\t')
 
