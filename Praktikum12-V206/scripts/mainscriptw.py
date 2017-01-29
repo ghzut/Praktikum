f4c17a001ef825a0711a82f7de91a61a6e96e4cd
Lukas Rolf <Lukas.Rolf@tu-dortmund.de>
2017-01-29 15:49:52 +0100

    Ynniks Kram.

--- Praktikum12-V206/scripts/mainscriptw.py
+++ Praktikum12-V206/scripts/mainscriptw.py
@@ -1,153 +1,141 @@
 from table import makeTable
-from bereich import bereich
 import numpy as np
 from scipy import stats
 from scipy.optimize import curve_fit
+from scipy.stats import linregress
 import matplotlib.pyplot as plt
 import uncertainties.unumpy as unp
 
-
-# BackwardsVNominal = []
-# BackwardsVStd = []
-# for value in BackwardsV:
-#     BackwardsVNominal.append(unp.nominal_values(value))
-#     BackwardsVStd.append(unp.std_devs(value))
-# BackwardsVNominal = np.array(BackwardsVNominal)
-# BackwardsVStd = np.array(BackwardsVStd)
-
-# makeTable([Gaenge, ForwardsVNominal*100, ForwardsVStd*100, BackwardsVNominal*100, BackwardsVStd*100], r'{'+r'Gang'+r'} & \multicolumn{2}{c}{'+r'$v_\text{v}/\si[per-mode=reciprocal]{\centi\meter\per\second}$'+r'} & \multicolumn{2}{c}{'+r'$v_\text{r}/\si[per-mode=reciprocal]{\centi\meter\per\second}$'+r'}', 'tabges', ['S[table-format=2.0]', 'S[table-format=2.3]', ' @{${}\pm{}$} S[table-format=1.3]', 'S[table-format=2.3]', ' @{${}\pm{}$} S[table-format=1.3]'], ["%2.0f", "%2.3f", "%2.3f", "%2.3f", "%2.3f"])
-
-# unp.uarray(np.mean(), stats.sem())
-
-# plt.cla
-# plt.clf
-# plt.plot(ForwardsVNominal*100, DeltaVForwardsNominal, 'gx', label='Daten mit Bewegungsrichtung aufs Mikrofon zu')
-# plt.plot(BackwardsVNominal*100, DeltaVBackwardsNominal, 'rx', label='Daten mit Bewegungsrichtung vom Mikrofon weg')
-# plt.ylim(0, line(t[-1], *params)+0.1)
-# plt.xlim(0, t[-1]*100)
-# plt.xlabel(r'$v/\si{\centi\meter\per\second}$')
-# plt.ylabel(r'$\Delta f / \si{\hertz}$')
-# plt.legend(loc='best')
-# plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-# plt.savefig('build/'+'VgegenDeltaV')
-
-# a = unp.uarray(params[0], np.sqrt(covar[0][0]))
+#Temparaturgraphen
+Pa, Pb, T1, T2, Leistung = np.genfromtxt("scripts/Daten", unpack=True)
+Pa = Pa+1
+Pa = Pa *100000
+Pb = Pb+1
+Pb = Pb *100000
+T1 += 273.15
+T2 += 273.15
+Zeitab = np.linspace(0,1080,len(T1))
+#Konstanten
+p0 = 5.51*10**(-3)#T = o, P = 1 Bar
+k = 1.14
+cWasser = 4183#j/(kg*K)
 
 
-# Messing Schmal 9 x 0.7 x 0.4 93+-12
-AMessingS = 0.007*0.004
-pMessingS = 8520
-cMessingS = 385
-kMessingS = 93
-print('p*c MessingS:',pMessingS*cMessingS)
+#Die einzelnen Approximationskurven im Vergleich
+def Polynom(x, A, B, C):
+    return A*x*x+B*x+C
 
-# Messing Breit 9 x 1.2 x 0.4
-AMessingB = 0.012*0.004
-pMessingB = 8520
-cMessingB = 385
-kMessingB = 93
-print('p*c MessingB:',pMessingB*cMessingB)
+def Bruch1(x, A, B, C):
+    return A/(1+B*(x**0.005))
 
-# Aluminium 9 x 1.2 x 0.4
-AAluminium = 0.012*0.004
-pAluminium = 2800
-cAluminium = 830
-kAluminium = 220
-print('p*c Aluminium:',pAluminium*cAluminium)
+def Bruch2(x, A, B, C, D):
+    return (A*x**C)/(1+B*x**C)+D
 
-# Edelstahl 9 x 1.2 x 0.4
-AEdlestahl = 0.012*0.004
-pEdelstahl = 8000
-cEdlestahl = 400
-kEdelstahl = 20
-print('p*c Edelstahl:',pEdelstahl*cEdlestahl)
+def linear(x, A, B):
+    return A*x+B
 
-# Abstand zwischen Thermoelementen an einem Stab
-AbT = 0.03
+#Fehler
+paramsPolynomT1, covariancePolynomT1 = curve_fit(Polynom, Zeitab, T1)
+paramsPolynomT2, covariancePolynomT2 = curve_fit(Polynom, Zeitab, T2)
+errorsPT1 = np.sqrt(np.diag(covariancePolynomT1))
+errorsPT2 = np.sqrt(np.diag(covariancePolynomT2))
 
-# Hauptscript
+print('Polynomapproximation T1')
+print('A =', paramsPolynomT1[0], 'pm', errorsPT1[0])
+print('B =', paramsPolynomT1[1], 'pm', errorsPT1[1])
+print('C =', paramsPolynomT1[2], 'pm', errorsPT1[2])
+print('Polynomapproximation T2')
+print('A =', paramsPolynomT2[0], 'pm', errorsPT2[0])
+print('B =', paramsPolynomT2[1], 'pm', errorsPT2[1])
+print('C =', paramsPolynomT2[2], 'pm', errorsPT2[2])
 
-x21, dT21 = np.genfromtxt("scripts/T2-T1", unpack=True)
-x78, dT78 = np.genfromtxt("scripts/T7-T8", unpack=True)
-x21, x78 = x21*1000/7.55, x78*1000/7.55
-dT21, dT78 = dT21*2.5/11.7, dT78*5/8.9
-def line(x, a, b):
-    return a*x+b
+#Fehlerarrays
+PolynomAT1 = unp.uarray(paramsPolynomT1[0], errorsPT1[0])
+PolynomBT1 = unp.uarray(paramsPolynomT1[1], errorsPT1[1])
+PolynomAT2 = unp.uarray(paramsPolynomT2[0], errorsPT2[0])
+PolynomBT2 = unp.uarray(paramsPolynomT2[1], errorsPT1[1])
 
-# Die zeitlichen Abstände berechnen (unnötig)
-dx21 = []
-dx21.append(x21[0])
-for i in range(len(x21)-1):
-    dx21.append(x21[i+1]-x21[i])
-dx21 = np.array(dx21)
-dx78 = []
-dx78.append(x78[0])
-for i in range(len(x78)-1):
-    dx78.append(x78[i+1]-x78[i])
-dx78 = np.array(dx78)
+plt.cla()
+plt.clf()
+x_plot = np.linspace(-10, 1100)
+#Graphenapproximation
+#Graphmit Temperaturen
+plt.plot(Zeitab, T1, 'rx', label ="Temperatur von Resservour 1")
+plt.plot(Zeitab, T2, 'gx', label = "Temperatur von Resservour 2")
+#plt.xlabel(r'$t/\si{\second}$')
+#plt.ylabel(r'$T / \si{\kelvin}$')
+plt.legend(loc="best")
+plt.savefig("Temperaturen.png")
 
-# Den Wärmestrom berechnen und ausgeben und Tabelle erstellen
-print('dT21/AbT:')
-print(dT21/AbT)
-print('dT78/AbT:')
-print(dT78/AbT)
-print('Wärmefluss T21')
-print(-dT21/AbT * AMessingB *kMessingB)
-print('Wärmefluss T78')
-print(-dT78/AbT * AEdlestahl *kEdelstahl)
+plt.cla()
+plt.clf()
+#Fitgraph von T1
+plt.plot(Zeitab, T1, 'rx', label ="Temperatur von Resservour 1")
+plt.plot(x_plot, Polynom(x_plot, *paramsPolynomT1), 'b-', label='Fit durch Polynom 2. Grades', linewidth=1)
+plt.legend(loc="best")
+plt.savefig("T1.png")
 
-makeTable([x21, dT21, -dT21/AbT * AMessingB *kMessingB], r'{$t/\si{\second}$} & {$(T2-T1)/\si{\kelvin}$} & {$\frac{\Delta Q_\text{21}}{\Delta t}/\si{\watt}$}', 'tabT21', [r'S[table-format=4.0]', r'S[table-format=1.1]', r'S[table-format=1.3]'], ["%4.0f", "%1.1f", "%0.3f"])
-makeTable([x78, dT78, -dT78/AbT * AEdlestahl *kEdelstahl], r'{$t/\si{\second}$} & {$(T7-T8)/\si{\kelvin}$} & {$\frac{\Delta Q_\text{78}}{\Delta t}/\si{\watt}$}', 'tabT78', [r'S[table-format=4.0]', r'S[table-format=1.1]', r'S[table-format=1.3]'], ["%4.0f", "%1.1f", "%0.3f"])
-
-# Berechnung von den kappas
-
-def kappa(Anah, Afern, deltat, p, c):
-	return (p*c*0.03**2) / (2*deltat*unp.log(Anah/Afern))
-
-AnahMessing, AfernMessing, deltatMessing = np.genfromtxt('scripts/Messing', unpack=True)
-AnahMessing *= 10/5.8 /2
-AfernMessing *= 10/5.8 /2
-deltatMessing *= 1000/14.95
-AnahAluminium, AfernAluminium, deltatAluminium = np.genfromtxt('scripts/Aluminium', unpack=True)
-AnahAluminium *= 10/5.8 /2
-AfernAluminium *= 10/5.8 /2
-deltatAluminium *= 1000/14.95 
-AnahEdelstahl, AfernEdelstahl, deltatEdelstahl = np.genfromtxt('scripts/Edelstahl', unpack=True)
-AnahEdelstahl *= 100/23
-AfernEdelstahl *= 100/23
-deltatEdelstahl *= 1000/11.9
-
-makeTable([AnahMessing, AfernMessing, deltatMessing], r'{$A_\text{nah}$} & {$A_\text{fern}$} & {$\Delta t / \si{\second}$}', 'tabMessing', [r'S[table-format=1.2]', r'S[table-format=1.2]', r'S[table-format=2.0]'], ["%1.2f", "%1.2f", "%2.0f"])
-makeTable([AnahAluminium, AfernAluminium, deltatAluminium], r'{$A_\text{nah}$} & {$A_\text{fern}$} & {$\Delta t / \si{\second}$}', 'tabAluminium', [r'S[table-format=1.1]', r'S[table-format=1.1]', r'S[table-format=1.1]'], ["%1.1f", "%1.1f", "%1.1f"])
-makeTable([AnahEdelstahl, AfernEdelstahl, deltatEdelstahl], r'{$A_\text{nah}$} & {$A_\text{fern}$} & {$\Delta t / \si{\second}$}', 'tabEdelstahl', [r'S[table-format=1.1]', r'S[table-format=1.1]', r'S[table-format=2.0]'], ["%1.1f", "%1.1f", "%2.0f"])
+plt.cla()
+plt.clf()
+#Graphtemparatur2
+plt.plot(Zeitab, T2, 'gx', label = "Temperatur von Resservour 2")
+plt.plot(x_plot, Polynom(x_plot, *paramsPolynomT2), 'b-', label='Fit durch Polynom 2. Grades', linewidth=1)
+plt.legend(loc="best")
+plt.savefig("T2.png")
 
 
-# Mittelwert + Standartabweichung des Mittelwertes Messing
-AnahMessing = unp.uarray(np.mean(AnahMessing), stats.sem(AnahMessing))
-print('AnahMessing:', AnahMessing)
-AfernMessing = unp.uarray(np.mean(AfernMessing), stats.sem(AfernMessing))
-print('AfernMessing:', AfernMessing)
-deltatMessing = unp.uarray(np.mean(deltatMessing), stats.sem(deltatMessing))
-print('deltatMessing:', deltatMessing)
-print('KappaMessing:', kappa(AnahMessing, AfernMessing, deltatMessing, pMessingB, cMessingB))
+#Güte bestimmen
+#Formel
+A2T1 = 2*PolynomAT1
+A2T2 = 2*PolynomAT2
+print('koeffableitungAT1',A2T1)
+print('koeffableitungAT2',A2T2)
 
-# Mittelwert + Standartabweichung des Mittelwertes Aluminium
-AnahAluminium = unp.uarray(np.mean(AnahAluminium), stats.sem(AnahAluminium))
-print('AnahAluminium:', AnahAluminium)
-AfernAluminium = unp.uarray(np.mean(AfernAluminium), stats.sem(AfernAluminium))
-print('AfernAluminium:', AfernAluminium)
-deltatAluminium = unp.uarray(np.mean(deltatAluminium), stats.sem(deltatAluminium))
-print('deltatAluminium:', deltatAluminium)
-print('KappaAluminium:', kappa(AnahAluminium, AfernAluminium, deltatAluminium, pAluminium, cAluminium))
+def Ableitung(x, A, B):
+    return A*x+B
 
-# Mittelwert + Standartabweichung des Mittelwertes Edelstahl
-AnahEdelstahl = unp.uarray(np.mean(AnahEdelstahl), stats.sem(AnahEdelstahl))
-print('AnahEdelstahl:', AnahEdelstahl)
-AfernEdelstahl = unp.uarray(np.mean(AfernEdelstahl), stats.sem(AfernEdelstahl))
-print('AfernEdelstahl:', AfernEdelstahl)
-deltatEdelstahl = unp.uarray(np.mean(deltatEdelstahl), stats.sem(deltatEdelstahl))
-print('deltatEdelstahl:', deltatEdelstahl)
-print('KappaEdelstahl:', kappa(AnahEdelstahl, AfernEdelstahl, deltatEdelstahl, pEdelstahl, cEdlestahl))
+
+#print('AbleitungT1',Ableitung(12*60,A2T1,PolynomBT1))
+#print('AbleitungT2',Ableitung(12*60,A2T2,PolynomBT2))
+def realGuete(Jim,m1,mkck,cw,N):
+    return (m1*cw+mkck)*Jim/N
+
+print('GueteT1bei 2',realGuete(Ableitung(Zeitab[4],A2T1,PolynomBT1),3,660,cWasser,Leistung[2]))
+print('GueteT1bei 8',realGuete(Ableitung(Zeitab[8],A2T1,PolynomBT1),3,660,cWasser,Leistung[8]))
+print('GueteT1bei 12',realGuete(Ableitung(Zeitab[12],A2T1,PolynomBT1),3,660,cWasser,Leistung[12]))
+print('GueteT1bei 16',realGuete(Ableitung(Zeitab[16],A2T1,PolynomBT1),3,660,cWasser,Leistung[16]))
+
+#ideal
+print(T1[4]/(T1[4]-T2[4]))
+print(T1[8]/(T1[8]-T2[8]))
+print(T1[12]/(T1[12]-T2[12]))
+print(T1[16]/(T1[16]-T2[16]))
+#Rechnung
+
+#Dampfdruckkurve L-Bestimmung
+Dampfdruck, covarianceDampfdruck = curve_fit(linear, T2, Pa)
+errorsDampfdruck = np.sqrt(np.diag(covarianceDampfdruck))
+DampfdruckA = unp.uarray(Dampfdruck[0], errorsDampfdruck[0])
+DampfdruckB = unp.uarray(Dampfdruck[1], errorsDampfdruck[1])
+#Graph
+plt.cla()
+plt.clf()
+Dampf_plot = np.linspace(273.15+20, 273.15+55, len(T1))
+plt.plot(T1, Pb, 'rx', label ="Druck gegen Temaratur")
+plt.plot(Dampf_plot, linear(Dampf_plot, *Dampfdruck), 'b-', label='linearer Fit', linewidth=3)
+plt.savefig("Dampdruck.png")
+
+print('Dampdrucksteigung',Dampfdruck[0],'pm',errorsDampfdruck[0])
+print('Achsenabschnitt',Dampfdruck[1],'pm',errorsDampfdruck[1])
 
 
 
+#Massendruchsatz
+def Massendurch(Jim2,m2,cw,mkck,L):
+    return (m2*cw+mkck)*Jim2/L
+
+print('Massendurchsatz4',Massendurch(Ableitung(4*60,A2T2,PolynomBT2),3,cWasser,660,-8.13*1000/18*Dampfdruck[0]))
+print('Massendurchsatz8',Massendurch(Ableitung(8*60,A2T2,PolynomBT2),3,cWasser,660,-8.13*1000/18*Dampfdruck[0]))
+print('Massendurchsatz12',Massendurch(Ableitung(12*60,A2T2,PolynomBT2),3,cWasser,660,-8.13*1000/18*Dampfdruck[0]))
+print('Massendurchsatz16',Massendurch(Ableitung(16*60,A2T2,PolynomBT2),3,cWasser,660,-8.13*1000/18*Dampfdruck[0]))
+#Kompressorleistung
