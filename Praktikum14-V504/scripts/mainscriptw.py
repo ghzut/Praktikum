a6a95ecb5c0338237a87fda359a47d7ce46f0abb
Yannik Brune <yannik.brune@tu-dortmund.de>
2017-04-23 20:39:54 +0200

    auswertung quasi fertig

--- Praktikum14-V504/scripts/mainscriptw.py
+++ Praktikum14-V504/scripts/mainscriptw.py
@@ -52,12 +52,12 @@ V3=V2
 
 
 #8a:
-plt.plot(V1wo2_5[0], V1wo2_5[1], 'g+', label='Kennlinie mit I = 2,0 A')
-plt.plot(V1wo2_5[0], V1wo2_5[2], 'go', label='Kennlinie mit I = 2,1 A')
-plt.plot(V1wo2_5[0], V1wo2_5[3], 'r+', label='Kennlinie mit I = 2,2 A')
-plt.plot(V1wo2_5[0], V1wo2_5[4], 'ro', label='Kennlinie mit I = 2,3 A')
-plt.plot(V1wo2_5[0], V1wo2_5[5], 'bo', label='Kennlinie mit I = 2,4 A')
-plt.plot(V1ol2_5[0], V1ol2_5[1], 'yo', label='Kennlinie mit I = 2,5 A')
+plt.plot(V1wo2_5[0], V1wo2_5[1], 'g+', label='I = 2,0 A')
+plt.plot(V1wo2_5[0], V1wo2_5[2], 'go', label='I = 2,1 A')
+plt.plot(V1wo2_5[0], V1wo2_5[3], 'r+', label='I = 2,2 A')
+plt.plot(V1wo2_5[0], V1wo2_5[4], 'ro', label='I = 2,3 A')
+plt.plot(V1wo2_5[0], V1wo2_5[5], 'bo', label='I = 2,4 A')
+plt.plot(V1ol2_5[0], V1ol2_5[1], 'yo', label='I = 2,5 A')
 #plt.ylim(0, line(t[-1], *params)+0.1)
 #plt.xlim(0, t[-1]*100)
 plt.xlabel(r'$U/\si{\volt}$')
@@ -69,15 +69,15 @@ plt.cla()
 plt.clf()
 
 print("yeah")
-logV1 = [[np.log(x) for x in V1ol2_5[0]],[np.log(x) for x in V1ol2_5[1]]]
+logV1 = [[np.log(x) for x in V1ol2_5[0]],[np.log(x*10**-6) for x in V1ol2_5[1]]]
 print(logV1)
-
+#richtige göße gefixt ^^
 #doppeltlogarithmischI=2.5
 plt.plot(logV1[0], logV1[1], 'yx', label ="logarithmische Darstellung der höchsten Kennlinie")
 #plt.ylim(0, line(t[-1], *params)+0.1)
 #plt.xlim(0, t[-1]*100)
-plt.xlabel(r'$U/\si{\volt}$')
-plt.ylabel(r'$I / \si{\micro\ampere}$')
+plt.xlabel(r'$\log (U/\si{\volt})$')
+plt.ylabel(r'$\log (I/\si{\ampere})$')
 plt.legend(loc='best')
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.savefig('build/'+'Kennlinielog')
@@ -106,11 +106,11 @@ logV2 = [np.log(V2[0]),V2[1]]
 print(logV2)
 
 #einfachlogarithmischI=2.5
-plt.plot(logV2[0], logV2[1], 'yx', label ="logarithmische Darstellung der höchsten Kennlinie V2")
+plt.plot(logV2[0], logV2[1], 'yx', label ="halblogarithmische Darstellung des Anlaufstromgebietes")
 #plt.ylim(0, line(t[-1], *params)+0.1)
 #plt.xlim(0, t[-1]*100)
 plt.xlabel(r'$U/\si{\volt}$')
-plt.ylabel(r'$I / \si{\micro\ampere}$')
+plt.ylabel(r'$\log (I / \si{\ampere})$')
 plt.legend(loc='best')
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.savefig('build/'+'KennlinielogV2')
@@ -151,16 +151,16 @@ print("nochmehrkram")
 print(tdletz)
 print(stderrletz)
 #1. Datentabelle
-makeTable([V1wo2_5[0],V1wo2_5[1],V1wo2_5[2],V1wo2_5[3],V1wo2_5[4]], r'{'+r'$U$'+r'} & {'+r'$I_1$'+r'} & {'+r'$I_2$'+r'} & {'+r'$I_3$'+r'} & {'+r'$I_4$'+r'}' ,'tabV1wo' , ['S[table-format=4.0]' , 'S[table-format=4.0]' , 'S[table-format=4.0]' , 'S[table-format=4.0]' , 'S[table-format=4.0]'] , ["%4.0f", "%4.0f","%4.0f","%4.0f","%4.0f"])
+makeTable([V1wo2_5[0],V1wo2_5[1],V1wo2_5[2],V1wo2_5[3],V1wo2_5[4]], r'{'+r'$U/\si{\volt}$'+r'} & {'+r'$I_\text{2,1}/\si{\micro\ampere}$'+r'} & {'+r'$I_\text{2,2}/\si{\micro\ampere}$'+r'} & {'+r'$I_\text{2,3}/\si{\micro\ampere}$'+r'} & {'+r'$I_\text{2,4}/\si{\micro\ampere}$'+r'}' ,'tabV1wo' , ['S[table-format=4.0]' , 'S[table-format=4.0]' , 'S[table-format=4.0]' , 'S[table-format=4.0]' , 'S[table-format=4.0]'] , ["%4.0f", "%4.0f","%4.0f","%4.0f","%4.0f"])
 
 #2.datentabelle
-makeTable([V1ol2_5[0] [1:len(V1ol2_5[0])//2],V1ol2_5[1] [1:len(V1ol2_5[0])//2] ], r'{'+r'$U$'+r'} & {'+r'$I$'+r'}' ,'tabV1ol1' , ['S[table-format=3.0]' , 'S[table-format=4.0]'] ,  ["%3.0f", "%4.0f"])
+makeTable([V1ol2_5[0] [0:len(V1ol2_5[0])//2],V1ol2_5[1] [0:len(V1ol2_5[0])//2] ], r'{'+r'$U/\si{\volt}$'+r'} & {'+r'$I_\text{2,5}/\si{\micro\ampere}$'+r'}' ,'tabV1ol1' , ['S[table-format=3.0]' , 'S[table-format=4.0]'] ,  ["%3.0f", "%4.0f"])
 
-makeTable([V1ol2_5[0][(len(V1ol2_5[0]))//2+1:(len(V1ol2_5[0]))-1] ,V1ol2_5[0][(len(V1ol2_5[0]))//2+1:(len(V1ol2_5[0]))-1]] , r'{'+r'$U$'+r'} & {'+r'$I$'+r'}' ,'tabV1ol2' , ['S[table-format=3.0]' , 'S[table-format=4.0]'] ,  ["%3.0f", "%4.0f"])
+makeTable([V1ol2_5[0][(len(V1ol2_5[0]))//2+1:(len(V1ol2_5[0]))] ,V1ol2_5[0][(len(V1ol2_5[0]))//2+1:(len(V1ol2_5[0]))]] , r'{'+r'$U/\si{\volt}$'+r'} & {'+r'$I_\text{2,5}/\si{\micro\ampere}$'+r'}' ,'tabV1ol2' , ['S[table-format=3.0]' , 'S[table-format=4.0]'] ,  ["%3.0f", "%4.0f"])
 
 #3.datentabelle
-makeTable([V3[0],V3[1]], r'{'+r'$U$'+r'} & {'+r'$I$'+r'}' ,'tabV2' , ['S[table-format=1.2]' , 'S[table-format=2.3]'] ,  ["%1.2f", "%2.3f"])
+makeTable([V3[0],V3[1]], r'{'+r'$U/\si{\volt}$'+r'} & {'+r'$I/\si{\nano\ampere}$'+r'}' ,'tabV2' , ['S[table-format=1.2]' , 'S[table-format=2.2]'] ,  ["%1.2f", "%2.2f"])
 
-makeTable([andereAngabenI[0:5],andereAngabenW[0:5],letzterkram], r'{'+r'$I_\text{f}$'+r'} & {'+r'$W_\text{f}$'+r'} &{'+r'$I_\text{S}$'+r'}' ,'IS' , ['S[table-format=3.0]', 'S[table-format=3.0]' , 'S[table-format=4.0]'] ,  ["%3.0f","%3.0f", "%4.0f"])
+makeTable([andereAngabenI[0:5],andereAngabenW[0:5],letzterkram], r'{'+r'$I_\text{f}/\si{\ampere}$'+r'} & {'+r'$W_\text{f}/\si{\watt}$'+r'} &{'+r'$I_\text{S}/\si{\micro\ampere}$'+r'}' ,'IS' , ['S[table-format=3.1]', 'S[table-format=3.1]' , 'S[table-format=4.0]'] ,  ["%3.1f","%3.1f", "%4.0f"])
 
-makeTable([andereAngabenI[0:5],andereAngabenW[0:5],andereheiztemp], r'{'+r'$I_\text{f}$'+r'} & {'+r'$W_\text{f}$'+r'} &{'+r'$T_\text{S}$'+r'}' ,'tabheiz' , ['S[table-format=3.2]', 'S[table-format=3.2]' , 'S[table-format=4.0]'] ,  ["%3.0f","%3.0f", "%4.0f"])
+makeTable([andereAngabenI[0:5],andereAngabenW[0:5],andereheiztemp], r'{'+r'$I_\text{f}/\si{\ampere}$'+r'} & {'+r'$W_\text{f}/\si{\watt}$'+r'} &{'+r'$T_\text{S}$'+r'}' ,'tabheiz' , ['S[table-format=3.1]', 'S[table-format=3.2]' , 'S[table-format=4.0]'] ,  ["%3.1f","%3.2f", "%4.0f"])
