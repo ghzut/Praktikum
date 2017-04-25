4f7c083ae2a139d206aa154680f1031a4c5ac93a
Lukas Rolf <Lukas.Rolf@tu-dortmund.de>
2017-04-25 20:37:57 +0200

    Rechnung und Graphen fast fertig Tabellen und Auswertungstext fehlen noch.

--- Praktikum15-V500/scripts/mainscriptw.py
+++ Praktikum15-V500/scripts/mainscriptw.py
@@ -1,4 +1,5 @@
 from table import makeTable
+from linregress import linregress
 from bereich import bereich
 import numpy as np
 from scipy import stats
@@ -6,6 +7,7 @@ from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 import uncertainties.unumpy as unp
 import scipy.constants as const
+
 
 # BackwardsVNominal = []
 # BackwardsVStd = []
@@ -34,151 +36,209 @@ import scipy.constants as const
 # plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 # plt.savefig('build/'+'VgegenDeltaV')
 
-# a = unp.uarray(params[0], np.sqrt(covar[0][0]))
+#funktionen
+def std(x):
+	return unp.std_devs(x)
+def nom(x):
+	return unp.nominal_values(x)
+
+
+# gegebene Werte
+Wellenlaengen = np.array([578, 546, 492, 435, 406, 365.5])*10**(-9)
+c = const.value('speed of light in vacuum')
+Frequenz = c/Wellenlaengen
+
 
 # Messwerte
+gelb = np.genfromtxt("scripts/gelb", unpack=True)
+gelb_alternativ = np.genfromtxt("scripts/gelb_alternativeDaten", unpack=True)
+gruen = np.genfromtxt("scripts/gruen", unpack=True)
+blaugruen = np.genfromtxt("scripts/blaugruen", unpack=True)
+violett = np.genfromtxt("scripts/violett", unpack=True)
+ultraV1 = np.genfromtxt("scripts/ultraV1", unpack=True)
+ultraV2 = np.genfromtxt("scripts/ultraV2", unpack=True)
 
-#Der erste Versuch 8a:
-V1wo2_5 = np.genfromtxt('scripts/DatenV1', unpack=True)
-print(V1wo2_5)
-print("Die letzte Durchführung bei 2.5A")
-V1ol2_5 = np.genfromtxt("scripts/DatenV1_2.5",unpack=True)
-print(V1ol2_5)
-#Der zweite Versuch 8c:
-print("Der zweite Versuch")
-V2 = np.genfromtxt("scripts/DatenV2",unpack=True)
-print(V2)
-V3=V2
+# In Standarteinheiten umwandeln
+gelb[0] *= 10**(-3)
+gelb[1] *= 10**(-12)
+gelb_alternativ[0] *= 10**(-3)
+gelb_alternativ[1] *= 10**(-12)
+gruen[0] *= 10**(-3)
+gruen[1] *= 10**(-12)
+blaugruen[0] *= 10**(-3)
+blaugruen[1] *= 10**(-12)
+violett[0] *= 10**(-3)
+violett[1] *= 10**(-12)
+ultraV1[0] *= 10**(-3)
+ultraV1[1] *= 10**(-12)
+ultraV2[0] *= 10**(-3)
+ultraV2[1] *= 10**(-12)
 
-
-#8a:
-plt.plot(V1wo2_5[0], V1wo2_5[1], 'g+', label='I = 2,0 A')
-plt.plot(V1wo2_5[0], V1wo2_5[2], 'go', label='I = 2,1 A')
-plt.plot(V1wo2_5[0], V1wo2_5[3], 'r+', label='I = 2,2 A')
-plt.plot(V1wo2_5[0], V1wo2_5[4], 'ro', label='I = 2,3 A')
-plt.plot(V1wo2_5[0], V1wo2_5[5], 'bo', label='I = 2,4 A')
-plt.plot(V1ol2_5[0], V1ol2_5[1], 'yo', label='I = 2,5 A')
+#a)
+Nullstellen = []
+params, covar = linregress(gelb[0][gelb[0]>=0], np.sqrt(gelb[1][gelb[0]>=0]))
+params = unp.uarray(params, covar[0:-1])
+Nullstelle = -params[1]/params[0]
+Nullstellen += [Nullstelle]
+print('Ugrenzgelb: ', Nullstelle)
+plt.cla()
+plt.clf()
+plt.plot(gelb[0][gelb[0]>=0], np.sqrt(gelb[1][gelb[0]>=0]), 'yx', label='Daten')
+x = np.linspace(nom(Nullstelle)*1.02,  0.1, 1000)
+plt.plot(x, nom(params[0])*x+nom(params[1]), 'r', label='linearer Fit')
 #plt.ylim(0, line(t[-1], *params)+0.1)
-#plt.xlim(0, t[-1]*100)
+plt.xlim(x[0], x[-1])
 plt.xlabel(r'$U/\si{\volt}$')
-plt.ylabel(r'$I / \si{\micro\ampere}$')
+plt.ylabel(r'$\sqrt{I / \si{\ampere}}$')
 plt.legend(loc='best')
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/'+'Kennlinien')
+plt.savefig('build/'+'sqrtIgegenUgelb')
+
+params, covar = linregress(gruen[0][gruen[0]>=0], np.sqrt(gruen[1][gruen[0]>=0]))
+params = unp.uarray(params, covar[0:-1])
+Nullstelle = -params[1]/params[0]
+Nullstellen += [Nullstelle]
+print('Ugrenzgruen: ', Nullstelle)
 plt.cla()
 plt.clf()
-
-
-print("yeah")
-logV1 = [[np.log(x) for x in V1ol2_5[0]],[np.log(x*10**-6) for x in V1ol2_5[1]]]
-print(logV1)
-#intressanterBereich
-x42 = np.linspace(logV1[0][1],logV1[0][-2],1000)
-def linfunc(x,a,b):
-    return a*x+b
-#m = logV2[0][6]
-#vermuteter Gültigkeitsbereich
-lin = curve_fit(linfunc,logV1[0][6:18],logV1[1][6:18])
-
-linspast=lin
-lin = [lin[0],np.sqrt(np.diag(lin[1]))]
-lin = unp.uarray(lin[0],lin[1])
-print(x42)
-
-
-#richtige göße gefixt ^^
-#doppeltlogarithmischI=2.5
-plt.plot(logV1[0], logV1[1], 'yx', label ="logarithmische Darstellung der höchsten Kennlinie")
-plt.plot(x42,x42*linspast[0][0]+linspast[0][1],label = "gefittete Ausgleichsgerade")
+plt.plot(gruen[0][gruen[0]>=0], np.sqrt(gruen[1][gruen[0]>=0]), 'yx', label='Daten')
+x = np.linspace(nom(Nullstelle)*1.02,  0.1, 1000)
+plt.plot(x, nom(params[0])*x+nom(params[1]), 'r', label='linearer Fit')
 #plt.ylim(0, line(t[-1], *params)+0.1)
-#plt.xlim(0, t[-1]*100)
-plt.xlabel(r'$\log (U/\si{\volt})$')
-plt.ylabel(r'$\log (I/\si{\ampere})$')
-plt.legend(loc='best')
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/'+'Kennlinielog')
-plt.cla()
-plt.clf()
-
-#8c:
-V2 = np.array([V2[0]+V2[1]*0.001,V2[1]*10**(-9)])
-print(V2)
-
-
-print("yeah")
-logV2 = [V2[0],np.log(V2[1])]
-print(logV2)
-
-
-
-linV2 = curve_fit(linfunc,logV2[0],logV2[1])
-linV3 = linV2
-print("hi")
-linV2 = [linV2[0],np.sqrt(np.diag(linV2[1]))]
-linV2 = unp.uarray(linV2[0],linV2[1])
-print(linV2)
-Heiztemp = - const.value("electron volt")/(linV2[0]*const.value("Boltzmann constant"))
-print ("Stevae")
-print(Heiztemp)
-print ("Stevae")
-Heiztemp45 = Heiztemp
-x2 = np.linspace(logV2[0][0],logV2[0][-1],1000)
-
-#einfachlogarithmischI=2.5
-plt.plot(logV2[0], logV2[1], 'yx', label ="halblogarithmische Darstellung des Anlaufstromgebietes")
-plt.plot(x2,linV3[0][0]*x2+linV3[0][1],label = "gefittete Ausgleichsgerade")
-#plt.ylim(0, line(t[-1], *params)+0.1)
-#plt.xlim(0, t[-1]*100)
+plt.xlim(x[0], x[-1])
 plt.xlabel(r'$U/\si{\volt}$')
-plt.ylabel(r'$\log (I / \si{\ampere})$')
+plt.ylabel(r'$\sqrt{I / \si{\ampere}}$')
 plt.legend(loc='best')
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/'+'KennlinielogV2')
+plt.savefig('build/'+'sqrtIgegenUgruen')
+
+params, covar = linregress(blaugruen[0][blaugruen[0]>=0], np.sqrt(blaugruen[1][blaugruen[0]>=0]))
+params = unp.uarray(params, covar[0:-1])
+Nullstelle = -params[1]/params[0]
+Nullstellen += [Nullstelle]
+print('Ugrenzblaugruen: ', Nullstelle)
 plt.cla()
 plt.clf()
+plt.plot(blaugruen[0][blaugruen[0]>=0], np.sqrt(blaugruen[1][blaugruen[0]>=0]), 'yx', label='Daten')
+x = np.linspace(nom(Nullstelle)*1.02,  0.1, 1000)
+plt.plot(x, nom(params[0])*x+nom(params[1]), 'r', label='linearer Fit')
+#plt.ylim(0, line(t[-1], *params)+0.1)
+plt.xlim(x[0], x[-1])
+plt.xlabel(r'$U/\si{\volt}$')
+plt.ylabel(r'$\sqrt{I / \si{\ampere}}$')
+plt.legend(loc='best')
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/'+'sqrtIgegenUblaugruen')
 
-#8d:
-andereAngabenU = np.linspace(4.0,5.75,6)
-andereAngabenI = [2.0,2.1,2.2,2.3,2.4,2.5]
-andereAngabenW = andereAngabenI*andereAngabenU
-andereAngabenW = andereAngabenW - 1
-andereheiztemp = andereAngabenW/(5.7*10**(-12)*0.32*0.28)
-andereheiztemp = andereheiztemp**(0.25)
-print(andereheiztemp)
-#8e:
-Heiztemp = np.array(andereheiztemp[0:-1])
-letzterkram = np.array([V1wo2_5[1][25],V1wo2_5[2][25],V1wo2_5[3][25],1500,3000])*10**(-6)
-meh = (const.value("Planck constant")/const.value("Boltzmann constant"))*(const.value("Planck constant")/const.value("Boltzmann constant"))*(const.value("Planck constant")/const.value("electron volt"))/(0.000032*const.value("electron mass"))
-print("meh")
-print(meh)
-letz = -np.log(meh*letzterkram/(4*np.pi*(Heiztemp**2)))
-print(letz)
-letz = letz *const.value("Boltzmann constant")*Heiztemp/const.value("electron volt")
-print(letzterkram)
-print(letz)
-tdletz = np.mean(letz)
-stderrletz = stats.sem(letz)
-print("nochmehrkram")
-print(tdletz)
-print(stderrletz)
-lin = lin[0]
-print(lin)
-dtrez = unp.uarray(tdletz,stderrletz)
-print(dtrez)
-print(Heiztemp45)
-#1. Datentabelle
-makeTable([V1wo2_5[0],V1wo2_5[1],V1wo2_5[2],V1wo2_5[3],V1wo2_5[4], V1wo2_5[5]], r'{'+r'$U/\si{\volt}$'+r'} & {'+r'$I_\text{2,0}/\si{\micro\ampere}$'+r'} & {'+r'$I_\text{2,1}/\si{\micro\ampere}$'+r'} & {'+r'$I_\text{2,2}/\si{\micro\ampere}$'+r'} & {'+r'$I_\text{2,3}/\si{\micro\ampere}$'+r'} & {'+r'$I_\text{2,4}/\si{\micro\ampere}$}' ,'tabV1wo' , ['S[table-format=3.0]' , 'S[table-format=3.0]' , 'S[table-format=3.0]' , 'S[table-format=3.0]' , 'S[table-format=4.0]', 'S[table-format=4.0]'] , ["%4.0f", "%4.0f","%4.0f","%4.0f","%4.0f","%4.0f"])
+params, covar = linregress(violett[0][violett[0]>=0], np.sqrt(violett[1][violett[0]>=0]))
+params = unp.uarray(params, covar[0:-1])
+Nullstelle = -params[1]/params[0]
+Nullstellen += [Nullstelle]
+print('Ugrenzviolett: ', Nullstelle)
+plt.cla()
+plt.clf()
+plt.plot(violett[0][violett[0]>=0], np.sqrt(violett[1][violett[0]>=0]), 'yx', label='Daten')
+x = np.linspace(nom(Nullstelle)*1.02,  0.1, 1000)
+plt.plot(x, nom(params[0])*x+nom(params[1]), 'r', label='linearer Fit')
+#plt.ylim(0, line(t[-1], *params)+0.1)
+plt.xlim(x[0], x[-1])
+plt.xlabel(r'$U/\si{\volt}$')
+plt.ylabel(r'$\sqrt{I / \si{\ampere}}$')
+plt.legend(loc='best')
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/'+'sqrtIgegenUviolett')
 
-#2.datentabelle
-makeTable([V1ol2_5[0] [0:len(V1ol2_5[0])//2],V1ol2_5[1] [0:len(V1ol2_5[0])//2] ], r'{'+r'$U/\si{\volt}$'+r'} & {'+r'$I_\text{2,5}/\si{\micro\ampere}$'+r'}' ,'tabV1ol1' , ['S[table-format=3.0]' , 'S[table-format=4.0]'] ,  ["%3.0f", "%4.0f"])
+params, covar = linregress(ultraV1[0][ultraV1[0]>=0], np.sqrt(ultraV1[1][ultraV1[0]>=0]))
+params = unp.uarray(params, covar[0:-1])
+Nullstelle = -params[1]/params[0]
+Nullstellen += [Nullstelle]
+print('UgrenzultraV1: ', Nullstelle)
+plt.cla()
+plt.clf()
+plt.plot(ultraV1[0][ultraV1[0]>=0], np.sqrt(ultraV1[1][ultraV1[0]>=0]), 'yx', label='Daten')
+x = np.linspace(nom(Nullstelle)*1.02,  0.1, 1000)
+plt.plot(x, nom(params[0])*x+nom(params[1]), 'r', label='linearer Fit')
+#plt.ylim(0, line(t[-1], *params)+0.1)
+plt.xlim(x[0], x[-1])
+plt.xlabel(r'$U/\si{\volt}$')
+plt.ylabel(r'$\sqrt{I / \si{\ampere}}$')
+plt.legend(loc='best')
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/'+'sqrtIgegenUultraV1')
 
-makeTable([V1ol2_5[0][(len(V1ol2_5[0]))//2+1:(len(V1ol2_5[0]))] ,V1ol2_5[1][(len(V1ol2_5[0]))//2+1:(len(V1ol2_5[0]))]] , r'{'+r'$U/\si{\volt}$'+r'} & {'+r'$I_\text{2,5}/\si{\micro\ampere}$'+r'}' ,'tabV1ol2' , ['S[table-format=3.0]' , 'S[table-format=4.0]'] ,  ["%3.0f", "%4.0f"])
+params, covar = linregress(ultraV2[0][ultraV2[0]>=0], np.sqrt(ultraV2[1][ultraV2[0]>=0]))
+params = unp.uarray(params, covar[0:-1])
+Nullstelle = -params[1]/params[0]
+Nullstellen += [Nullstelle]
+print('UgrenzultraV2: ', Nullstelle)
+plt.cla()
+plt.clf()
+plt.plot(ultraV2[0][ultraV2[0]>=0], np.sqrt(ultraV2[1][ultraV2[0]>=0]), 'yx', label='Daten')
+x = np.linspace(nom(Nullstelle)*1.02,  0.1, 1000)
+plt.plot(x, nom(params[0])*x+nom(params[1]), 'r', label='linearer Fit')
+#plt.ylim(0, line(t[-1], *params)+0.1)
+plt.xlim(x[0], x[-1])
+plt.xlabel(r'$U/\si{\volt}$')
+plt.ylabel(r'$\sqrt{I / \si{\ampere}}$')
+plt.legend(loc='best')
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/'+'sqrtIgegenUultraV2')
 
-#3.datentabelle
-makeTable([V3[0],V3[1]], r'{'+r'$U/\si{\volt}$'+r'} & {'+r'$I/\si{\nano\ampere}$'+r'}' ,'tabV2' , ['S[table-format=1.2]' , 'S[table-format=2.2]'] ,  ["%1.2f", "%2.2f"])
+# b)
+params, covar = linregress(Frequenz, nom(Nullstellen))
+params = unp.uarray(params, covar[0:-1])
+print('e0/h berechnet: ', params[0])
+print('e0/h lit: ', const.value('Planck constant')/const.value('elementary charge'))
+print('Austrittsarbeit in eV: ', params[1])
+plt.cla()
+plt.clf()
+plt.plot(Frequenz, nom(Nullstellen), 'yx', label='Daten')
+x = np.linspace(Frequenz[0]-1000,  Frequenz[-1]+1000, 1000)
+plt.plot(x, nom(params[0])*x+nom(params[1]), 'r', label='linearer Fit')
+#plt.ylim(0, line(t[-1], *params)+0.1)
+plt.xlim(x[0], x[-1])
+plt.xlabel(r'$f/\si{\per\second}$')
+plt.ylabel(r'$U_\text{g}/\si{\volt}$')
+plt.legend(loc='best')
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/'+'Ugegennu')
 
-makeTable([andereAngabenI[0:5],andereAngabenW[0:5],letzterkram*10**6], r'{'+r'$I_\text{f}/\si{\ampere}$'+r'} & {'+r'$W_\text{f}/\si{\watt}$'+r'} &{'+r'$I_\text{S}/\si{\micro\ampere}$'+r'}' ,'IS' , ['S[table-format=3.1]', 'S[table-format=3.1]' , 'S[table-format=4.0]'] ,  ["%3.1f","%3.1f", "%4.0f"])
+# c)
+plt.cla()
+plt.clf()
+plt.plot(gelb[0], np.sqrt(gelb[1]), 'yx', label='Daten')
+#plt.ylim(0, line(t[-1], *params)+0.1)
+#plt.xlim(x[0], x[-1])
+plt.xlabel(r'$U/\si{\volt}$')
+plt.ylabel(r'$\sqrt{I / \si{\ampere}}$')
+plt.legend(loc='best')
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/'+'sqrtIgegenUgelbtest')
 
-makeTable([andereAngabenI[0:5],andereAngabenW[0:5],andereheiztemp], r'{'+r'$I_\text{f}/\si{\ampere}$'+r'} & {'+r'$W_\text{f}/\si{\watt}$'+r'} &{'+r'$T_\text{S}$'+r'}' ,'tabheiz' , ['S[table-format=3.1]', 'S[table-format=3.2]' , 'S[table-format=4.0]'] ,  ["%3.1f","%3.2f", "%4.0f"])
 
-makeTable([[unp.nominal_values(lin)],[unp.std_devs(lin)],[unp.nominal_values(Heiztemp45)],[unp.std_devs(Heiztemp45)],[unp.nominal_values(dtrez)],[unp.std_devs(dtrez)]], r'\multicolumn{2}{c}{$a_\text{\SI{2.5}{\ampere}}$} & \multicolumn{2}{c}{$T_\text{\SI{2.5}{\ampere}}$}/\si{\kelvin} & \multicolumn{2}{c}{$E/\si{\electronvolt}$}', 'tabs',[r'S[table-format=1.3]',  r'@{${}\pm{}$} S[table-format=1.3]',r'S[table-format=4.0]', r'@{${}\pm{}$} S[table-format=2.0]', r' S[table-format=1.2]', r'@{${}\pm{}$} S[table-format=1.2]'], ["%1.3f", "%1.3f", "%4.0f", "%2.0f", "%1.2f", "%1.2f"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
