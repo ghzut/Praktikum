f4915781864036f4e58338381865224457a374c3
Lukas Rolf <Lukas.Rolf@tu-dortmund.de>
2017-05-26 14:56:20 +0200

    Kram

--- Praktikum19-US1/scripts/mainscriptw.py
+++ Praktikum19-US1/scripts/mainscriptw.py
@@ -1,5 +1,6 @@
 ﻿from table import makeTable
 from table import makeNewTable
+from linregress import *
 from customFormatting import *
 from bereich import bereich
 from weightedavgandsem import weighted_avg_and_sem
@@ -46,5 +47,44 @@ import scipy.constants as const
 # a = unp.uarray(params[0], np.sqrt(covar[0][0]))
 # params = unp.uarray(params, np.sqrt(np.diag(covar)))
 
-Abmessungen=np.genfromtxt('scripts/zylinderdaten.txt',unpack=True)
-np.genfromtxt('scripts/a.txt',unpack=True)
+
+#print(sum([1,2,3]))
+
+Abmessungen=np.genfromtxt('scripts/zylinderdaten.txt')
+ErsteMessung=np.genfromtxt('scripts/a.txt',unpack=True)
+ZweiteMessung=np.genfromtxt('scripts/b.txt')
+DritteMessung=np.genfromtxt('scripts/c.txt',unpack=True)
+VierteMessung=np.genfromtxt('scripts/d.txt')
+
+#Umrechnen
+ErsteMessung=[ErsteMessung[0]/10**6, ErsteMessung[1], 10**(ErsteMessung[2]/20)]
+ZweiteMessung=ZweiteMessung/10**6
+DritteMessung=[(DritteMessung[0]+26.96)/10**6, DritteMessung[1], 10**(DritteMessung[2]/20)]
+
+
+#berechnung der Schallgeschwindigkeiten
+params=linregress(ErsteMessung[0],Abmessungen[0:-2]*2)
+paramsGes1=unp.uarray(*params[0:-1])
+print('Parameter bei der Berechnung der Schallgeschwindigkeit mit Impuls-Echo-Verfahren\n',paramsGes1)
+params=linregress(ZweiteMessung,Abmessungen[0:-2])
+paramsGes2=unp.uarray(*params[0:-1])
+print('Parameter bei der Berechnung der Schallgeschwindigkeit mit Durchschallungs-Verfahren\n',paramsGes2)
+
+#berechnung der Daempfungskonstanten
+x=np.linspace(0.05,0.25)
+params=linregress(Abmessungen[0:-2]*2,unp.log(ErsteMessung[1]/ErsteMessung[2]))
+paramsDaempfung=unp.uarray(*params[0:-1])
+print('Parameter bei der Berechnung der Daempfung mit Impuls-Echo-Verfahren\n',paramsDaempfung)
+plt.cla()
+plt.clf()
+plt.plot(x, np.exp(unp.nominal_values(paramsDaempfung[1]))*np.exp(unp.nominal_values(paramsDaempfung[0]) *x), 'b-', label='Augleichsfunktion')
+plt.plot(Abmessungen[0:-2]*2, (ErsteMessung[1]/ErsteMessung[2]), 'rx', label='Messwerte')
+# plt.ylim(0, line(t[-1], *params)+0.1)
+# plt.xlim(0, t[-1]*100)
+# plt.xlabel(r'$v/\si{\centi\meter\per\second}$')
+# plt.ylabel(r'$\Delta f / \si{\hertz}$')
+plt.legend(loc='best')
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/'+'test')
+
+
