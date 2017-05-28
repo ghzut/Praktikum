69e4eef00dbaf42d66b36afda9072852957a4da2
Lukas Rolf <Lukas.Rolf@tu-dortmund.de>
2017-05-28 20:19:37 +0200

    Alles außer Auswertung Cepstrum und Diskussion.

--- Praktikum19-US1/scripts/mainscriptw.py
+++ Praktikum19-US1/scripts/mainscriptw.py
@@ -57,27 +57,28 @@ DritteMessung=np.genfromtxt('scripts/c.txt',unpack=True)
 VierteMessung=np.genfromtxt('scripts/d.txt')
 
 #Umrechnen
-ErsteMessung=[ErsteMessung[0]/10**6, ErsteMessung[1], 10**((ErsteMessung[2])/20)]
+ErsteMessung=[ErsteMessung[0]/10**6, ErsteMessung[1], 10**((ErsteMessung[2]+10)/20)]
 ZweiteMessung=ZweiteMessung/10**6
-DritteMessung=[(DritteMessung[0])/10**6, DritteMessung[1], 10**((DritteMessung[2])/20)]
+DritteMessung=[(DritteMessung[0])/10**6, DritteMessung[1], 10**((DritteMessung[2]+10)/20)]
 #berechnungen
 
 #   berechnung der Schallgeschwindigkeiten
 params=linregress(ErsteMessung[0],Abmessungen[0:-2]*2)
 Ges1Std=params[-1]
 paramsGes1=unp.uarray(*params[0:-1])
-deltat1 = -paramsGes1[1]/paramsGes1[0]
+deltat1 = -paramsGes1[1]/paramsGes1[0]/2
 print('Parameter bei der Berechnung der Schallgeschwindigkeit mit Impuls-Echo-Verfahren\n',paramsGes1, deltat1, Ges1Std)
 params=linregress(ZweiteMessung,Abmessungen[0:-2])
 Ges2Std=params[-1]
 paramsGes2=unp.uarray(*params[0:-1])
-deltat2 = -paramsGes2[1]/paramsGes2[0]
+deltat2 = -paramsGes2[1]/paramsGes2[0]/2
 print('Parameter bei der Berechnung der Schallgeschwindigkeit mit Durchschallungs-Verfahren\n',paramsGes2, deltat2, Ges2Std)
 
 #   berechnung der Daempfungskonstanten
 params=linregress(Abmessungen[0:-2]*2,unp.log(ErsteMessung[1]/ErsteMessung[2]))
+DaempfungStd=params[-1]
 paramsDaempfung=unp.uarray(*params[0:-1])
-print('Parameter bei der Berechnung der Daempfung mit Impuls-Echo-Verfahren(Es sollte ca. 500 dB/m rauskommen)\n',paramsDaempfung*40/np.log(10))
+print('Parameter bei der Berechnung der Daempfung mit Impuls-Echo-Verfahren(Es sollte ca. 500 dB/m rauskommen)\n',paramsDaempfung*2,paramsDaempfung*40/np.log(10),DaempfungStd)
 
 
 
@@ -120,16 +121,21 @@ plt.plot(x, unp.nominal_values(paramsDaempfung[1])+unp.nominal_values(paramsDaem
 plt.plot(Abmessungen[0:-2]*2, np.log(ErsteMessung[1]/ErsteMessung[2]), 'rx', label='Messwerte')
 plt.xlim(x[0], x[-1])
 plt.xlabel(r'$x/\si{\meter}$')
-plt.ylabel(r'$\log(U/\si{\volt})$')
+plt.ylabel(r'$\log(U\text{n}/\si{\volt})$')
 plt.legend(loc='best')
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.savefig('build/'+'UgegenX')
 
 #tabellen
 #makeNewTable([Abmessungen], r'{$l/\si[per-mode=reciprocal]{\meter}$}','Abmessungen')
-makeNewTable([convert(100*Abmessungen[0:-2], floatFormat),convert(10**6*ErsteMessung[0], floatFormat), convert(ErsteMessung[1], floatFormat), convert(np.log10(ErsteMessung[2]) *20, floatFormat)], r'{$l/\si[per-mode=reciprocal]{\centi\meter}$} & {$T/\si[per-mode=reciprocal]{\micro\second}$} & {$U/\si[per-mode=reciprocal]{\volt}$} & {$\text{TGC}/\si{\decibel}$}','a', [r'S[table-format=2.3]',r' S[table-format=2.2]',r' S[table-format=1.3]',r' S[table-format=2.2]'],[r'{:2.3f}',r'{:2.2f}',r'{:1.3f}',r'{:2.2f}'])
+makeNewTable([convert(100*Abmessungen[0:-2], floatFormat),convert(10**6*ErsteMessung[0], floatFormat), convert(ErsteMessung[1], floatFormat), convert(np.log10(ErsteMessung[2]) *20-10, floatFormat), convert(ErsteMessung[1]/ErsteMessung[2], floatFormat)], r'{$l/\si[per-mode=reciprocal]{\centi\meter}$} & {$T/\si[per-mode=reciprocal]{\micro\second}$} & {$U/\si[per-mode=reciprocal]{\volt}$} & {$\text{TGC}/\si{\decibel}$} & {$U_\text{n}/\si[per-mode=reciprocal]{\volt}$}','a', [r'S[table-format=2.3]',r' S[table-format=2.2]',r' S[table-format=1.3]',r' S[table-format=2.2]',r' S[table-format=1.2e1]'],[r'{:2.3f}',r'{:2.2f}',r'{:1.3f}',r'{:2.2f}',r'{:1.2E}'])
 makeNewTable([convert(100*Abmessungen[0:-2], floatFormat),convert(10**6*ZweiteMessung, floatFormat)], r'{$l/\si[per-mode=reciprocal]{\centi\meter}$} & {$T/\si[per-mode=reciprocal]{\micro\second}$}','b',[r'S[table-format=2.3]',r' S[table-format=2.2]'],[r'{:2.3f}',r'{:2.2f}'])
-makeNewTable([convert(10**6*DritteMessung[0], floatFormat)], r'{$\Delta T/\si[per-mode=reciprocal]{\micro\second}$}','c',[r'S[table-format=2.3]'])# r'{:1.2e}')
-#makeNewTable([convert(VierteMessung, floatFormat)], r'{$T/\si[per-mode=reciprocal]{\second}$}','d',['S'], [r'{:1.2e}'])
+makeNewTable([convert(10**6*DritteMessung[0], floatFormat)], r'{$\Delta T/\si[per-mode=reciprocal]{\micro\second}$}','c',[r'S[table-format=2.2]'])# r'{:1.2e}')
+makeNewTable([convert(VierteMessung, floatFormat),convert(VierteMessung-2*deltat2*10**6, unpFormat)], r'{$T/\si[per-mode=reciprocal]{\micro\second}$} & {$T_\text{I}/\si[per-mode=reciprocal]{\micro\second}$}','d',['S[table-format=2.2]',r'S[table-format=2.1,table-figures-uncertainty=1]'], [r'{:1.2f}',r'{:1.2f}'])
 
+print('Abstände im Augenmodell:')
+print((VierteMessung-2*deltat2*10**6)[0]*10**-6 *1410 /2)
+print((VierteMessung[1]-VierteMessung[0])*10**-6 *1410 /2)
+print((VierteMessung[2]-VierteMessung[1])*10**-6 *2500 /2)
+print((VierteMessung[3]-VierteMessung[2])*10**-6 *1410 /2)
 
