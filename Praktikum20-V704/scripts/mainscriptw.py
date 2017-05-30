db106606dd82b6f9aa2c8e0e51a450f846c7f2a5
Lukas Rolf <lukas.rolf@tu-dortmund.de>
2017-05-30 15:47:49 +0200

    Kram.

---
+++ Praktikum20-V704/scripts/mainscriptw.py
@@ -0,0 +1,48 @@
+﻿from table import makeTable
+from table import makeNewTable
+from customFormatting import *
+from bereich import bereich
+from weightedavgandsem import weighted_avg_and_sem
+from weightedavgandsem import avg_and_sem
+import numpy as np
+from scipy import stats
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+import uncertainties.unumpy as unp
+import scipy.constants as const
+
+# BackwardsVNominal = []
+# BackwardsVStd = []
+# for value in BackwardsV:
+#     BackwardsVNominal.append(unp.nominal_values(value))
+#     BackwardsVStd.append(unp.std_devs(value))
+# BackwardsVNominal = np.array(BackwardsVNominal)
+# BackwardsVStd = np.array(BackwardsVStd)
+
+# einfacher:
+# BackwardsVNominal = unp.nominal_values(BackwardsV)
+# BackwardsVStd = unp.std_devs(BackwardsV)
+
+# makeTable([Gaenge, ForwardsVNominal, ForwardsVStd, ], r'{Gang} & \multicolumn{2}{c}{$v_\text{v}/\si[per-mode=reciprocal]{\centi\meter\per\second}$} & ', 'name', ['S[table-format=2.0]', 'S[table-format=2.3]', ' @{${}\pm{}$} S[table-format=1.3]', ], ["%2.0f", "%2.3f", "%2.3f",])
+
+#[per-mode=reciprocal],[table-format=2.3,table-figures-uncertainty=1]
+
+# unp.uarray(np.mean(), stats.sem())
+# unp.uarray(*avg_and_sem(values)))
+# unp.uarray(*weighted_avg_and_sem(unp.nominal_values(bneuDiff), 1/unp.std_devs(bneuDiff)))
+
+# plt.cla()
+# plt.clf()
+# plt.plot(ForwardsVNominal*100, DeltaVForwardsNominal, 'gx', label='Daten mit Bewegungsrichtung aufs Mikrofon zu')
+# plt.plot(BackwardsVNominal*100, DeltaVBackwardsNominal, 'rx', label='Daten mit Bewegungsrichtung vom Mikrofon weg')
+# plt.ylim(0, line(t[-1], *params)+0.1)
+# plt.xlim(0, t[-1]*100)
+# plt.xlabel(r'$v/\si{\centi\meter\per\second}$')
+# plt.ylabel(r'$\Delta f / \si{\hertz}$')
+# plt.legend(loc='best')
+# plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+# plt.savefig('build/'+'VgegenDeltaV')
+
+# a = unp.uarray(params[0], np.sqrt(covar[0][0]))
+# params = unp.uarray(params, np.sqrt(np.diag(covar)))
+
